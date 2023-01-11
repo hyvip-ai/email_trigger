e25b5161524d150a679f6a0cc70d0c077116a8c8
rajatcbd <rm2932002@gmail.com>
2023-01-12 04:59:47 +0530

    Message extract

--- pages/api/watchGmail.js
+++ pages/api/watchGmail.js
@@ -1,42 +1,54 @@
-// watchGmailMessages = async (event) => {
-//   try {
-//     await auth.auth.requireAuth(null, null, email);
-//   } catch (err) {
-//     console.log('An error has occurred in the auth process.');
-//     throw err;
-//   }
-//   const authClient = await auth.auth.authedUser.getClient();
-//   google.options({ auth: authClient });
+import { gmail } from './AuthClient';
 
-//   // Process the incoming message.
-//   const message = await getMostRecentMessageWithTag(email, historyId);
-//   if (message) {
-//     const messageInfo = extractInfoFromMessage(message);
-//     if (messageInfo.attachmentId && messageInfo.attachmentFilename) {
-//       const attachment = await extractAttachmentFromMessage(
-//         email,
-//         messageInfo.messageId,
-//         messageInfo.attachmentId
-//       );
-//       const topLabels = await analyzeAttachment(
-//         attachment.data.data,
-//         messageInfo.attachmentFilename
-//       );
-//       await updateReferenceSheet(
-//         messageInfo.from,
-//         messageInfo.attachmentFilename,
-//         topLabels
-//       );
-//     }
-//   }
-// };
+const getMostRecentMessageWithTag = async (email, historyId) => {
+  // Look up the most recent message.
+  const listMessagesRes = await gmail.users.messages.list({
+    userId: email,
+    maxResults: 1,
+  });
+  const messageId = await checkForDuplicateNotifications(
+    listMessagesRes.data.messages[0].id
+  );
+
+  // Get the message using the message ID.
+  if (messageId) {
+    const message = await gmail.users.messages.get({
+      userId: email,
+      id: messageId,
+    });
+
+    return message;
+  }
+};
+
+const extractInfoFromMessage = (message) => {
+  const messageId = message.data.id;
+  let from;
+
+  const headers = message.data.payload.headers;
+  for (var i in headers) {
+    if (headers[i].name === 'From') {
+      from = headers[i].value;
+    }
+  }
+
+  return {
+    messageId: messageId,
+    from: from,
+  };
+};
 
 export default async function handler(req, res) {
   const data = Buffer.from(req.body.message.data, 'base64').toString();
   const newMessageNotification = JSON.parse(data);
   const email = newMessageNotification.emailAddress;
   const historyId = newMessageNotification.historyId;
-  console.log(email);
-  console.log(historyId);
+  const message = await getMostRecentMessageWithTag(email, historyId);
+
+  if (message) {
+    const messageInfo = extractInfoFromMessage(message);
+    console.log({ message, ...messageInfo });
+  }
+
   res.status(200).json({ something: 'something' });
 }
