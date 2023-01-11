72de198604d4b3faf9cc4fd72cc5e10b832ec95a
rajatcbd <rm2932002@gmail.com>
2023-01-12 04:48:10 +0530

    Wacthing messages

--- pages/api/watchGmail.js
+++ pages/api/watchGmail.js
@@ -1,10 +1,4 @@
 // watchGmailMessages = async (event) => {
-//   // Decode the incoming Gmail push notification.
-//   const data = Buffer.from(event.data, 'base64').toString();
-//   const newMessageNotification = JSON.parse(data);
-//   const email = newMessageNotification.emailAddress;
-//   const historyId = newMessageNotification.historyId;
-
 //   try {
 //     await auth.auth.requireAuth(null, null, email);
 //   } catch (err) {
@@ -12,21 +6,37 @@
 //     throw err;
 //   }
 //   const authClient = await auth.auth.authedUser.getClient();
-//   google.options({auth: authClient});
+//   google.options({ auth: authClient });
 
 //   // Process the incoming message.
 //   const message = await getMostRecentMessageWithTag(email, historyId);
 //   if (message) {
 //     const messageInfo = extractInfoFromMessage(message);
 //     if (messageInfo.attachmentId && messageInfo.attachmentFilename) {
-//       const attachment = await extractAttachmentFromMessage(email, messageInfo.messageId, messageInfo.attachmentId);
-//       const topLabels = await analyzeAttachment(attachment.data.data, messageInfo.attachmentFilename);
-//       await updateReferenceSheet(messageInfo.from, messageInfo.attachmentFilename, topLabels);
+//       const attachment = await extractAttachmentFromMessage(
+//         email,
+//         messageInfo.messageId,
+//         messageInfo.attachmentId
+//       );
+//       const topLabels = await analyzeAttachment(
+//         attachment.data.data,
+//         messageInfo.attachmentFilename
+//       );
+//       await updateReferenceSheet(
+//         messageInfo.from,
+//         messageInfo.attachmentFilename,
+//         topLabels
+//       );
 //     }
 //   }
 // };
 
 export default async function handler(req, res) {
-  console.log(req);
+  const data = Buffer.from(req.body.message.data, 'base64').toString();
+  const newMessageNotification = JSON.parse(data);
+  const email = newMessageNotification.emailAddress;
+  const historyId = newMessageNotification.historyId;
+  console.log(email);
+  console.log(historyId);
   res.status(200).json({ something: 'something' });
 }
