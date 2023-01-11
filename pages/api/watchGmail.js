442237a78d20522810ab80a5b6fba7e778236e62
rajatcbd <rm2932002@gmail.com>
2023-01-12 04:41:35 +0530

    WatchGmail fixed

--- pages/api/watchGmail.js
+++ pages/api/watchGmail.js
@@ -26,7 +26,7 @@
 //   }
 // };
 
-export async function handler(req, res) {
+export default async function handler(req, res) {
   console.log(req);
   res.status(200).json({ something: 'something' });
 }
