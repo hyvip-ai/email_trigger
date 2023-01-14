5231e72d0ddaf37cc12650de280e5026653612aa
rajatcbd <rm2932002@gmail.com>
2023-01-14 05:55:23 +0530

    asdkja

--- pages/api/watchGmail.js
+++ pages/api/watchGmail.js
@@ -116,44 +116,41 @@ export default async function handler(req, res) {
   );
 
   if (!needed['Subject']?.includes('Re:')) {
-    const predefinedLines = Object.keys(tokens.replyTypes).map((item) => ({
-      subjectLine: replyTypes[item].emailSubject,
-      replyType: replyTypes[item].reply,
-    }));
-    let promises = [];
-    for (let i = 0; i < predefinedLines.length; i++) {
-      promises.push(
-        checkIfSame(
-          predefinedLines[i],
-          needed['Subject'],
-          predefinedLines[i].replyType
-        )
-      );
-    }
-    const matches = await Promise.all(promises);
-    let replyType = matches.find((item) => item.matches)?.replyType;
+    console.log(tokens.replyTypes);
+    // const predefinedLines = Object.keys(tokens.replyTypes).map((item) => ({
+    //   subjectLine: replyTypes[item].emailSubject,
+    //   replyType: replyTypes[item].reply,
+    // }));
+    // let promises = [];
+    // for (let i = 0; i < predefinedLines.length; i++) {
+    //   promises.push(
+    //     checkIfSame(
+    //       predefinedLines[i],
+    //       needed['Subject'],
+    //       predefinedLines[i].replyType
+    //     )
+    //   );
+    // }
+    // const matches = await Promise.all(promises);
+    // let replyType = matches.find((item) => item.matches)?.replyType;
 
-    if (matches) {
-      let reply = await generateReply(needed['Subject'], replyType);
-      reply = reply.replace(/^\s+|\s+$/g, '').trim();
-      try {
-        await createDraft({
-          access_token: tokens.access_token,
-          refresh_token: tokens.refresh_token,
-          threadId,
-          toEmail: needed['To'],
-          fromEmail: needed['From'],
-          reply,
-          subject: needed['Subject'],
-        });
-      } catch (err) {
-        console.log(err);
-      }
-    }
+    // if (matches) {
+    //   let reply = await generateReply(needed['Subject'], replyType);
+    //   reply = reply.replace(/^\s+|\s+$/g, '').trim();
+    //   try {
+    //     await createDraft({
+    //       access_token: tokens.access_token,
+    //       refresh_token: tokens.refresh_token,
+    //       threadId,
+    //       toEmail: needed['To'],
+    //       fromEmail: needed['From'],
+    //       reply,
+    //       subject: needed['Subject'],
+    //     });
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // }
   }
   res.status(200).json({ message: 'successful' });
 }
-
-// reply manner
-
-// `${subjectOfEmail} reply this in ${replyManner} in ${wordCount} words`;
--- pages/email.jsx
+++ pages/email.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../utils/supabase';
 
 function Email() {
-  const [state, setState] = useState('');
+  const [returnTypes, setReturnTypes] = useState(null);
+
+  const handleFetchAutomation = async () => {
+    const {
+      data: { replyTypes },
+    } = await supabase
+      .from('tokens')
+      .select('replyTypes')
+      .eq('email', 'rm2932002@gmail.com')
+      .single();
+    setReturnTypes(replyTypes);
+  };
 
   const handleWatch = async () => {
     await fetch('/api/watch').then((res) => res.json());
@@ -9,7 +21,12 @@ function Email() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // setNickname for the user
   };
+
+  useEffect(() => {
+    handleFetchAutomation();
+  }, []);
 
   return (
     <>
